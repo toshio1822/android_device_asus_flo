795288fb3d37b71a969d82a8549d0109bb983296
Nate Behary <nathan.behary@gmail.com>
2016-12-09 17:55:36 -0600

    fix for widevine

--- aosp_flo.mk
+++ aosp_flo.mk
@@ -35,5 +35,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/asus/flo/device.mk)
-$(call inherit-product-if-exists, vendor/asus/flo/device-vendor.mk)
+$(call inherit-product-if-exists, vendor/asus/flo/flo-vendor.mk)
 #$(call inherit-product-if-exists, vendor/qcom/proprietary/common/config/device-vendor.mk)
