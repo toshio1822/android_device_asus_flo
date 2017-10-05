14d647c5cc782540bf5fb888cf90310dfeca8f1c
followmsi <followmsi@gmail.com>
2017-10-05 22:41:19 +0200

    flo: configure and build HIDL DRM service

--- device-common.mk
+++ device-common.mk
@@ -322,7 +322,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-	android.hardware.drm@1.0-impl
+	android.hardware.drm@1.0-impl \
+	android.hardware.drm@1.0-service
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	wifi.interface=wlan0 \
