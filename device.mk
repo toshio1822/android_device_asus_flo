07470f5d6e0051c69f0aa1296430b3aa360d2f79
followmsi <followmsi@gmail.com>
2019-01-23 23:02:16 +0100

    Revert "flo: Enforce RRO on framework-res"
    This reverts commit c1a555c288bb906b98499f3123a3e4ca858830ee.

--- device.mk
+++ device.mk
@@ -36,6 +36,3 @@ $(call inherit-product, device/asus/flo/device-common.mk)
 $(call inherit-product-if-exists, vendor/asus/flo/device-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS := device/asus/flo/overlay
-
-PRODUCT_ENFORCE_RRO_TARGETS := \
-    framework-res
