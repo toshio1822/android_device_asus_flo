3c8a82ef79813c7b86d2b9539631c2bafd027715
followmsi <followmsi@gmail.com>
2019-10-11 14:10:37 +0200

    flo: fix DRM
    - remove drm service and make it passthrough

--- device-common.mk
+++ device-common.mk
@@ -82,8 +82,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-impl
 
 # Graphics
 PRODUCT_PACKAGES += \
